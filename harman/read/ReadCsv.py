c2ab83e9bdf57df08f8f1b1615878e2c85a8a1a6
Vishal Rathod <vishal.rathod@mattermotor.com>
2024-02-19 21:57:37 +0530

    add method to drop columns.

--- harman/read/ReadCsv.py
+++ harman/read/ReadCsv.py
@@ -9,5 +9,11 @@ class ReadCsv:
     def parse_date_column(self, column_name):
         self.data[column_name] = pd.to_datetime(self.data[column_name])
 
+    def drop_columns(self, *cols):
+        self.data.drop(*cols, axis=1, inplace=True)
+
+    def get_data(self):
+        return self.data
+
 
 
